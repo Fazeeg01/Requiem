982804a026c3670c11c2f59c267a4f7691101ffc
Fazeeg01 <145467817+Fazeeg01@users.noreply.github.com>
2024-05-24 06:42:17 +0300

    Add files via upload

---
+++ Data/DBInitializer.cs
@@ -0,0 +1,54 @@
+﻿using GameStore.Models;
+using System.Linq;
+
+namespace GameStore.Data
+{
+    public class DBInitializer
+    {
+        public static void Initialize(GameStoreContext context)
+        {
+            //context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            if (context.Roles.Any())
+            {
+                return;
+            }
+
+
+            //Ролі
+            var roles = new Role[]
+            {
+                new Role{ Name = "Модератор"},
+                new Role{ Name = "Користувач"}
+            };
+            
+            foreach (Role r in roles)
+            {
+                context.Roles.Add(r);
+            }
+            context.SaveChanges();
+
+
+            //Ролі
+            var genres = new Genre[]
+            {
+                new Genre{ Name = "Електромеханічні"},
+                new Genre{ Name = "Електронні"},
+                new Genre{ Name = "Клавішні"},
+                new Genre{ Name = "Духові"},
+                new Genre{ Name = "Самозвучні ударно-клавішні"}
+            };
+
+            foreach (Genre g in genres)
+            {
+                context.Genres.Add(g);
+            }
+            context.SaveChanges();
+
+
+           
+
+        }
+    }
+}
---
+++ Data/GameStoreContext.cs
@@ -0,0 +1,38 @@
+﻿using GameStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Data
+{
+    public class GameStoreContext : DbContext
+    {
+        public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Instrument> Games{ get; set; }
+        public DbSet<Genre> Genres{ get; set; }
+        public DbSet<Order> Orders{ get; set; }
+        public DbSet<Raiting> Raitings{ get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Screenshot> Screenshots{ get; set; }
+        public DbSet<User> Users{ get; set; }
+        public DbSet<Requirement> Requirements{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comment>().ToTable("Comment").HasOne(x=>x.User).WithMany(x=>x.Comments).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Instrument>().ToTable("Game");
+            modelBuilder.Entity<Genre>().ToTable("Genre");
+            modelBuilder.Entity<Order>().ToTable("Order").HasOne(x=>x.User).WithMany(x=>x.Orders).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Raiting>().ToTable("Raiting").HasOne(x => x.User).WithMany(x => x.Raitings).OnDelete(DeleteBehavior.Restrict); ;
+            modelBuilder.Entity<Role>().ToTable("Role");
+            modelBuilder.Entity<Screenshot>().ToTable("Screenshot");
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Requirement>().ToTable("Requirement");
+        }
+
+        public DbSet<GameStore.Models.News> News { get; set; }
+    }
+
+}
